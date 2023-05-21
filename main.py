397d5e00a05a6166a7836a32f60650c1b3d6676b
Bezonce <bezonce.szabo@gmail.com>
2023-05-21 12:33:53 +0200

    Fix for function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def myFunction_v1():
-  print("Hello from a function")
+    print("Hello from a function")
   
- def my_sum(a, b)
-  return a+b
+ def my_sum(a, b):
+    return a+b
 
 if __name__ == "__main__":
-  myFunction_v1()
-  print(mysum(5, 10))
+    myFunction_v1()
+    print(mysum(5, 10))

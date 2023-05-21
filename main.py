2080b75560544831e9748b11bb2af3e85bedff1e
Bezonce <bezonce.szabo@gmail.com>
2023-05-21 12:50:34 +0200

    Add .gitignore, requirements.txt, src/main.py, and test/test_main.py
    - Add .gitignore file to specify ignored files and directories
    - Remove main.py file
    - Add requirements.txt file to track project dependencies
    - Add src/main.py file with the main project code
    - Add test/test_main.py file with unit tests for the main code

--- main.py
+++
@@ -1,9 +0,0 @@
-def myFunction_v1():
-    print("Hello from a function")
-  
- def my_sum(a, b):
-    return a+b
-
-if __name__ == "__main__":
-    myFunction_v1()
-    print(mysum(5, 10))
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def my_sum(a, b):
+    return a + b
+
+
+def vec_diff_length(a, b):
+    return np.linalg.norm(a - b)
+
+
+if __name__ == "__main__":
+    print(my_sum(5, 10))
+    print(vec_diff_length(np.array([1, 1]), np.array([2, 2])))
